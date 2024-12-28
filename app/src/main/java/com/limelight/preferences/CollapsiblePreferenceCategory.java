9aa42d384c18ea90cf9d31a8d0c7607cce9be327
qy_ma <maqiyue0228@163.com>
2024-12-28 21:38:47 +0800

    解决设置界面加了折叠后图标乱飞问题

--- app/src/main/java/com/limelight/preferences/CollapsiblePreferenceCategory.java
+++ app/src/main/java/com/limelight/preferences/CollapsiblePreferenceCategory.java
@@ -1,6 +1,8 @@
 package com.limelight.preferences;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,11 +11,18 @@ import androidx.preference.PreferenceViewHolder;
 import com.limelight.R;
 
 public class CollapsiblePreferenceCategory extends PreferenceCategory {
-    private boolean isExpanded = false;
+    private boolean isExpanded;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     public CollapsiblePreferenceCategory(Context context, AttributeSet attrs) {
         super(context, attrs);
         setLayoutResource(R.layout.preference_category_collapsible);
+        
+        isExpanded = getKey() != null && getKey().equals("category_basic_settings");
+        
+        handler.post(() -> {
+            updateExpandState();
+        });
     }
 
     @Override
@@ -30,8 +39,6 @@ public class CollapsiblePreferenceCategory extends PreferenceCategory {
             updateExpandState();
             arrow.setRotation(isExpanded ? 0 : -90);
         });
-        
-        updateExpandState();
     }
 
     private void updateExpandState() {
